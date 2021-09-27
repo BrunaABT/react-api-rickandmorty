80bae9f37fe41cf065e3ede25725bbf3cc9eb43e
Bruna Barrón <barron.bruna@gmail.com>
2021-09-27 19:03:25 -0300

    feat: styles for Home page and CharacterItem page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,17 @@
 import React from "react";
 import CharacterItem from "../../components/CharacterItem";
 import style from "./styles.module.css";
+import logo from "../../assets/images/logo.png";
 
 function Home() {
   return (
     <div>
-      <h1 className={style.title}>meu site com react!!!</h1>
+      <img className={style.logo} src={logo} alt="" />
+      <h1 className={style.title}>Characters from Rick and Morty</h1>
+      <CharacterItem name="rick" />
+      <CharacterItem name="rick" />
+      <CharacterItem name="rick" />
+      <CharacterItem name="rick" />
       <CharacterItem name="rick" />
     </div>
   );
